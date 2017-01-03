284f64641bae8afb097f39a2e838a9b76fa20c94
Clément Serrano <clementserrano3@gmail.com>
2017-01-03 17:13:59 +0100

    Removing comments

--- src/anthill/Program.java
+++ src/anthill/Program.java
@@ -1,22 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package anthill;
 
-/**
- *
- * @author serra
- */
 public class Program {
 
-    /**
-     * @param args the command line arguments
-     */
-//BITE
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello");
     }
     
