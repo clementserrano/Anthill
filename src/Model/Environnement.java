a090e1ecf4b99f334b7588a775ea5341d4d26b68
Clément Serrano <clementserrano3@gmail.com>
2017-01-12 11:04:55 +0100

    Je fais

--- src/Model/Environnement.java
+++ src/Model/Environnement.java
@@ -10,6 +10,13 @@ public class Environnement {
 
     public Environnement(int hauteur, int largeur){
         this.grille = new Cellule[hauteur][largeur];
+        for (int i = 0; i < grille.length; i++){
+            for (int j = 0; j < grille[i].length ; j++){
+                if(i==0 || j == 0 || i == grille.length-1 ){
+                    grille[i][j] = new Obstacle(i,j);
+                }
+            }
+        }
         for(int i = 0; i < 3; i++ ){
             fourmis[i] = new Fourmi(i, grille);
         }
