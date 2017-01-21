203f55c4bfa3e5fe1a84a1825df8af9d2ebeaa72
Clément Serrano <clementserrano3@gmail.com>
2017-01-21 16:21:13 +0100

    Correctif readfile

--- src/Anthill/Program.java
+++ src/Anthill/Program.java
@@ -11,9 +11,9 @@ public class Program {
 
     public static void main(String[] args) throws IOException {
 
+        System.out.println(System.getProperty("user.dir" ));
         Readfile level = new Readfile();
-        level.Read("test.txt");
-        
+        level.Read("src\\Anthill\\util\\test.txt");
         Environnement evt = new Environnement(9,9,3,1,5);
         evt.run();
     }
