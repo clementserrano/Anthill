3d7ac2adef941da5dc447c53612a1cefbd22d708
Clément Serrano <clementserrano3@gmail.com>
2017-01-23 15:09:12 +0100

    Constrcuteur GUI

--- src/Anthill/view/GUI.java
+++ src/Anthill/view/GUI.java
@@ -1,9 +1,11 @@
 package Anthill.view;
 
+import javax.swing.JFrame;
+
 /**
  * Interface graphique représentant en temps réel la simulation
  * @author clementserrano
  */
 public class GUI {
-    
+    JFrame fen = new JFrame();
 }
